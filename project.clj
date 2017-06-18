85edb12ea421ff9ad88f97c56026d8f98f736891
Dave Harlan <david.k.harlan@gmail.com>
2017-06-18 18:19:16 -0400

    Add ring, ring-defaults, compojure, and hiccup

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [ring "1.6.1"]
+                 [ring/ring-defaults "0.3.0"]
+                 [compojure "1.6.0"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot a2-clojure-todo-app.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
