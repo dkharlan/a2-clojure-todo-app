6c541324a87e29eb7104c426274076c5ac89e06e
Dave Harlan <david.k.harlan@gmail.com>
2017-06-18 18:45:50 -0400

    Add some dummy templates, route config and Jetty helper function

--- src/a2_clojure_todo_app/core.clj
+++ src/a2_clojure_todo_app/core.clj
@@ -1,7 +1,36 @@
 (ns a2-clojure-todo-app.core
-  (:gen-class))
+  (:gen-class)
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [hiccup.core :refer [html]]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ring.adapter.jetty :refer [run-jetty]]))
+
+(defn index []
+  [:html
+   [:head
+    [:title "To Do"]]
+   [:body
+    [:p "Hello, world!"]]])
+
+(defn not-found []
+  [:html
+   [:head
+    [:title "Not Found"]]
+   [:body
+    [:p "Sorry, the page you're looking for does not exist."]]])
+
+(defroutes app-routes
+  (GET "/" [] (html (index)))
+  (route/not-found (html (not-found))))
+
+(def app (wrap-defaults app-routes site-defaults))
+
+(defn run! [app port]
+  (run-jetty app {:port port :join? false}))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
